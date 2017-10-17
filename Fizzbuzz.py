303f1671ed8b49c7c06dd240d05f2b6355cc9c92
Alexandre Batista <alexandrebatista84@gmail.com>
2017-10-17 16:00:02 +0100

    fizzbuzz

---
+++ Fizzbuzz.py
@@ -0,0 +1,22 @@
+i=1
+
+while i <= 100:
+
+    if i % 3 == 0 :
+        print ("fizz")
+        i+=1
+
+    elif i % 5 == 0:
+        print ("buzz")
+        i+=1
+
+    elif i % 3 == 0 & i % 5 == 0:
+        print ("fizzbuzz")
+        i+=1
+    
+    else: 
+        print(i)
+        i+=1
+    
+    
+
